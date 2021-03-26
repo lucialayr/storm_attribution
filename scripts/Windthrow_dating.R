b834eaeef0ebb67f63735cc49f4ef7dfce9d910e
lucialenasophie <lucia.sophie.layritz@posteo.de>
2021-03-26 22:56:03 +0100

    plotting function for stroms done, for disturbances work in progress

--- scripts/Windthrow_dating.R
+++ scripts/Windthrow_dating.R
@@ -5,6 +5,9 @@ library(stringr)
 library(purrr)
 library(benford)
 library(rgeos)
+library(ggplot2)
+library(ggnewscale)
+library(exactextractr)
 
 wd = "C:/Users/Lucia/Documents/Diss/Projects/Storm_Attribution/"
 setwd(wd)
@@ -160,11 +163,11 @@ mosaic_map_of_max_windspeed = function(Year, Threshold) {
     y_poly = st_read(path_poly)
     for ( i in c(1:length(date_vector))) {
       mask = y_poly[y_poly$layer == i,]
-      wind_speed = raster::mask(x[[1]], mask)
-      setZ(wind_speed, date_vector[i], name = "date")
-      print(getZ(wind_speed))
+      print("mask created")
+      wind_speed = raster::mask(x[[i]], mask)
+      print("masked")
       if (exists("wind_mosaic") == TRUE){
-        wind_mosaic = merge(wind_mosaic, wind_speed)
+        wind_mosaic = stack(wind_mosaic, wind_speed)
       } else {
       wind_mosaic = wind_speed
       }
@@ -172,35 +175,131 @@ mosaic_map_of_max_windspeed = function(Year, Threshold) {
   } else {
     print("loop entered, x < 1")
     wind_mosaic = x
-    setZ(wind_mosaic, date_vector, name = "date")
   }
-  print(getZ(wind_mosaic))
+  names(wind_mosaic) = date_vector
+  wind_mosaic = setZ(wind_mosaic, date_vector)
   return(wind_mosaic)
 }
 
 
 
-y_poly = st_read("data/WISC/wisc_storm_per_pixel/2000.shp")
-mask_test = y_poly[y_poly$layer == 1,]
-mask = raster::mask(x_test[[1]], mask_test)
-stack_test = raster::stack(x_test, y_rcl)
 
-assign_windspeed = function(z) {
-  for (i in c(1:4)) {
-    if (z[[5]] == as.numeric(str_sub(as.character(names(z)[i]), 2, 10))) {
-      z[[5]] = z[[i]]
-    }
+
+plot_gg_wind_speed = function(Year) { #untested!!
+  base_map = st_read("data/europe_countries.shp")
+  europe_outline = st_union(base_map)
+  
+  path_raster = paste0("data/WISC/mosaic/mosaic_",Year, ".grd")
+  mosaic = raster::stack(path_raster) %>% mask(base_map)
+  
+  path_poly = paste0("data/WISC/wisc_storm_per_pixel/", Year , ".shp")
+  outline = st_read(path_poly) 
+  
+  st_crs(outline) = st_crs(base_map)
+  outline =  st_intersection(outline, st_union(base_map))
+  
+  date_vector = as.numeric(str_sub(as.character(names(mosaic)), 2, 10))
+  plot = ggplot() + 
+    geom_sf(data = europe_outline, fill = "dimgrey") + theme_bw() +
+    geom_sf(data = outline, fill = NA)
+  colors_light = c("#14F57D", "#FFD500", "#65FC24", "#2347FD", "#FF5500", "#14F57D", "#FFD500", "#65FC24", "#2347FD", "#FF5500")
+  colors_low = c("#4DB57D", "#BD9756", "#84C668", "#5667BD", "#DF8558", "#4DB57D", "#BD9756", "#84C668", "#5667BD", "#DF8558")
+  for (i in c(1:length(date_vector))) {
+    pts = rasterToPoints(mosaic[[i]], spatial = TRUE)
+    # Then to a 'conventional' dataframe
+    df  = data.frame(pts)
+    print(head(df))
+    print(names(mosaic)[i])
+    plot = plot + 
+      geom_tile(data = df , aes_string(x = "x", y = "y", alpha = names(mosaic)[i]), color = NA, fill = colors_low[i]) +
+      scale_alpha(limits = c(20, 30), range= c(0, .9), na.value = .9)+
+      ggnewscale::new_scale("alpha")+
+      geom_tile(data = df , aes_string(x = "x", y = "y", alpha = names(mosaic)[i]), color = NA, fill = colors_light[i]) +
+      scale_alpha(limits = c(30,  maxValue(mosaic[[i]])), range= c(0, 1), na.value = 0) + ggnewscale::new_scale("alpha")
   }
+  plot = plot + theme(legend.position = "none")
+  return(plot)
 }
 
-ws_test = assign_windspeed(stack_test)
+plot_rasterized_disturbance = function(Year) { 
+  base_map = st_read("data/europe_countries.shp")
+  plot = ggplot() + geom_sf(data = base_map, fill = "dimgrey") + theme(legend.position = "none")
+  
+  colors_light = c("#14F57D", "#FFD500", "#65FC24", "#2347FD", "#FF5500", "#14F57D", "#FFD500", "#65FC24", "#2347FD", "#FF5500")
+  colors_low = c("#4DB57D", "#BD9756", "#84C668", "#5667BD", "#DF8558", "#4DB57D", "#BD9756", "#84C668", "#5667BD", "#DF8558")
+  
+  path_raster = paste0("data/WISC/mosaic/mosaic_",Year, ".grd")
+  mosaic = raster::stack(path_raster) %>% mask(base_map)
+  date_vector = as.numeric(str_sub(as.character(names(mosaic)), 2, 10))
+  
+  dates = read.csv("data/WISC/WISC_proj/date.txt", header = FALSE, col.names = "date")
+  # create table with years
+  dates = dates %>% 
+    dplyr::mutate(year = as.numeric(str_sub(as.character(date), 1, 4)),
+                  month = as.numeric(str_sub(as.character(date), 5, 6)),
+                  hyear = ifelse(month < 5, year,year + 1))
+  
+  Country = list("albania", "austria", "belarus", "belgium", "bosniaherzegovina")#,
+                 #"bulgaria", "croatia", "czechia", "denmark", "estonia",
+                 #"finland", "france", "germany", "greece", "hungary", 
+                 #"ireland", "italy", "latvia", "lithuania", "macedonia", 
+                 #"moldova", "montenegro", "netherlands", "norway", "poland", 
+                 #"portugal", "romania", "serbia", "slovakia", "slovenia", 
+                 #"spain", "sweden", "switzerland", "ukraine", "unitedkingdom")
+  print("set-up done")
+  
+  for (c in c(1:length(Country))) {
+    disturbance_path = paste0("data/dated_patches/windthrow_", Country[c], "_dated.shp" )
+    disturbance_poly = st_read(disturbance_path)
+    
+    for (i in c(1:length(date_vector))) {
+      if (date_vector[i] %in% unique(disturbance_poly$storm_date) == TRUE) {
+        disturbance_storm = disturbance_poly[disturbance_poly$storm_date == date_vector[i],] %>%
+          filter(!st_is_empty(.)) %>% st_union()
+        
+        template = mosaic[[i]]
+        
+        print("prepared rasteriztation")
+        disturbance_rast = exactextractr::coverage_fraction(template, disturbance_storm)
+        print("exactextract")
+        print(disturbance_rast[[1]])
+        
+        #data prep plot
+        pts = rasterToPoints(disturbance_rast[[1]], spatial = TRUE)
+        df  = data.frame(pts)
+        print(head(df))
+        
+        plot = plot + 
+          geom_tile(data = df , aes_string(x = "x", y = "y", alpha = "layer"), color = NA, fill = colors_low[i]) +
+          scale_alpha(limits = c(0, 0.001), range= c(0, .9), na.value = .9)+
+          ggnewscale::new_scale("alpha")+
+          geom_tile(data = df , aes_string(x = "x", y = "y", alpha = "layer"), color = NA, fill = colors_light[i]) +
+          scale_alpha(limits = c(0.001,  1), range= c(0, 1), na.value = 0) + ggnewscale::new_scale("alpha")
+      } else {"storm not in country"}
+    }
+    print(plot)
+  }
+  return(plot)
+  }
+    
 
-ws_test = raster::calc(stack_test, fun = assign_windspeed(stack_test))
+
+
+
 # EXECUTION
 
 for (a in c(2014:2017)) {
   write_polygon_storms(a, 20)
   
+}
+
+for (a in c(1896:2017)) {
+  test_path = paste0("data/WISC/wisc_storm_per_pixel/", a , ".shp")
+  if(file.exists(test_path)) {
+    mosaic = mosaic_map_of_max_windspeed(a, 20)
+    path = paste0("data/WISC/mosaic/mosaic_",a)
+    writeRaster(mosaic, path, overwrite = TRUE)
+  }
 }
 
 
